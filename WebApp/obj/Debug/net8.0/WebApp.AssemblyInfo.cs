c8a2bdc8774e0b04684d14607975a121e65fbcd7
zywiecj <jakub.zywiec@microsoft.wsei.edu.pl>
2024-11-06 10:08:11 +0100

    index

--- WebApp/obj/Debug/net8.0/WebApp.AssemblyInfo.cs
+++ WebApp/obj/Debug/net8.0/WebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be31d78082231fc79e401d6964767d421d568e6a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f160ff3e1be0732ced3b8942308ed4931f839cc")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
